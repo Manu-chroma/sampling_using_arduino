f85de3a81da1fb2dceb1ad6e165d5418e66bdc25
Manu-chroma <manvendra0310@gmail.com>
2015-11-28 19:01:00 +0530

    Increased Sampling Speed
    Speedup employed:
    Changing prescaler value from 128 to 16.
    Reading byte value (8-bit resolution)
    Average sampling rate achieved: 3.0 kHz
    Fluctuations between 2.6 kHz-4.1 kHz
    
    analogRefernce set to EXTERNAL

---
+++ Fast Sampling.ino
@@ -0,0 +1,29 @@
+
+#define FASTADC 1
+// defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+void setup() {
+  /*int start ;
+  int i ; */
+
+#if FASTADC
+  // set prescale to 16
+  sbi(ADCSRA, ADPS2) ;
+  cbi(ADCSRA, ADPS1) ;
+  cbi(ADCSRA, ADPS0) ;
+#endif
+  Serial.begin(115200) ;
+  analogReference(EXTERNAL);
+}
+
+void loop() {
+  while (1) {
+    Serial.println(byte(analogRead(A0)));
+  }
+}
